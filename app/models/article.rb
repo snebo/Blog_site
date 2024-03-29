6f72579827403df3ecd4de62fea1ca6ccb0415cd
Stefano <snebo54@hotmail.com>
2024-03-29 22:33:14 +0100

    created comment model with routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # valid ations for the input
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
